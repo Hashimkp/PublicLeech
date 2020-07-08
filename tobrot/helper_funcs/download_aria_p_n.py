51da87bd83301b608252557668e84c84ef1da95f
Hashimkp <44409027+Hashimkp@users.noreply.github.com>
2020-07-08 16:55:31 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -194,12 +194,8 @@ async def call_apropriate_function(
         message_to_send += local_file_name
         message_to_send += "</a>"
         message_to_send += "\n"
-    if message_to_send != "":
-        mention_req_user = f"<a href='tg://user?id={user_id}'>Your Requested Files</a>\n\n"
-        message_to_send = mention_req_user + message_to_send
-        message_to_send = message_to_send + "\n\n" + "#uploads"
-    else:
-        message_to_send = "<i>FAILED</i> to upload files. 😞😞"
+
+
     await sent_message_to_update_tg_p.reply_to_message.reply_text(
         text=message_to_send,
         quote=True,
